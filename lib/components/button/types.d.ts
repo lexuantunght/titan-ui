b9f35f981b2b21c7f678ce5ed85b1a1993619cf6
tunglx2 <tunglx2@vng.com.vn>
2023-05-04 16:23:51 +0700

    remove lib

--- lib/components/button/types.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    varriant: 'primary' | 'light' | 'error' | 'warning' | 'secondary';
-    mode: 'text' | 'outline' | 'fill' | 'link';
-    icon?: string;
-    loading?: boolean;
-}
